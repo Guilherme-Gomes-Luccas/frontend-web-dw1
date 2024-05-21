703cf763090a391990507a0cc5b7110c0ab77457
Guilherme-Gomes-Luccas <guilhermegomes.luccas@gmail.com>
2024-05-20 22:24:43 -0300

    Adicionando edição e exclusão de cards

--- if-finance/js/main.js
+++ if-finance/js/main.js
@@ -83,7 +83,7 @@ console.log(user[mostrar])
 console.log(user.email)
 console.log(user.endereco.rua) */
 
-const token = " " //adicione seu token
+const token = "cp1berhr01qu1k1i3hkgcp1berhr01qu1k1i3hl0" //adicione seu token
 
 const allStock = [
     {
@@ -141,14 +141,14 @@ const allStock = [
 function addCard({bolsa, codigo, empresa, valor, variacao, nAcoes}) {
     const listaCards = document.querySelector('body > main')
     listaCards.innerHTML = listaCards.innerHTML + `
-        <div class="card-ticker">
+        <div class="card-ticker" id=${codigo} onmouseenter="cardEnter(event)" onmouseleave="cardLeave(event)">,
             <header>
                 <h2><span>${bolsa}:</span> ${codigo}</h2>
                 <h1>${empresa}</h1>
             </header>
             <main>
-                <p>R$ ${realFormat(+valor/100)}</p>
-                <span ${variacao < 0 ? 'style="background: #FF0000;"' : ''}> ${variacao < 0 ? '▼' : '▲'} ${variacao}%</span>
+                <p data-valor="${valor}">R$ ${realFormat(+valor/100)}</p>
+                <span data-variacao="${variacao}" ${variacao < 0 ? 'style="background: #FF0000;"' : ''}> ${variacao < 0 ? '▼' : '▲'} ${variacao}%</span>
                 <span>R$ ${realFormat((valor/100) * (variacao/100))}</span>
             </main>
             <footer>
@@ -161,7 +161,49 @@ function addCard({bolsa, codigo, empresa, valor, variacao, nAcoes}) {
                     <span>Posição</span>
                 </div>
             </footer>
+            <div class="card-menu">
+                <span>Editar</span>
+                <span onclick="removeCard(event)">Excluir</span>
+            </div>
         </div>`
+
+        const allEdit = listaCards.querySelectorAll('.card-ticker .card-menu span:first-child')
+
+        allEdit.forEach(edit => {
+            edit.addEventListener('click', openEditModal)
+        })
+}
+
+function updateCard({bolsa, codigo, empresa, valor, variacao, nAcoes}) {
+    const card = document.querySelector('#'+codigo)
+    
+    card.innerHTML = `
+    <header>
+        <h2><span>${bolsa}:</span> ${codigo}</h2>
+        <h1>${empresa}</h1>
+    </header>
+    <main>
+        <p>R$ ${realFormat(+valor/100)}</p>
+        <span ${variacao < 0 ? 'style="background: #FF0000;"' : ''}> ${variacao < 0 ? '▼' : '▲'} ${variacao}%</span>
+        <span>R$ ${realFormat((valor/100) * (variacao/100))}</span>
+    </main>
+    <footer>
+        <div>
+            <p>${nAcoes}</p>
+            <span>Ações</span>
+        </div>
+        <div>
+            <p>R$ ${realFormat(nAcoes * (valor/100))}</p>
+            <span>Posição</span>
+        </div>
+    </footer>
+    <div class="card-menu">
+        <span>Editar</span>
+        <span onclick="removeCard(event)">Excluir</span>
+    </div>`
+
+    const edit = listaCards.querySelectorAll('.card-ticker .card-menu span:first-child')
+    edit.addEventListener('click', openEditModal)
 }
 
 function addTable({bolsa, codigo, empresa, setor, site, valor, variacao, nAcoes}){
@@ -258,21 +300,85 @@ const createApiCard = async (event) => {
 
     const {codigo, nAcoes} = event.target.elements
 
-    const response = await fetch(`https://finnhub.io/api/v1/quote?symbol=${codigo.value}&token=${token}`)
-    const result = await response.json()
+    try{
+        const response = await fetch(`https://finnhub.io/api/v1/quote?symbol=${codigo.value}&token=${token}`)
+        const result = await response.json()
 
-    const response2 = await fetch(`https://finnhub.io/api/v1/stock/profile2?symbol=${codigo.value}&token=${token}`)
-    const profile = await response2.json()
+        const response2 = await fetch(`https://finnhub.io/api/v1/stock/profile2?symbol=${codigo.value}&token=${token}`)
+        const profile = await response2.json()
 
-    addCard({
-        bolsa: profile.exchange.split(' ')[0],
-        codigo: codigo.value,
-        empresa: profile.name,
-        valor: result.c * 100,
-        variacao: result.d,
-        nAcoes: nAcoes.value
-    })
+        if (!response.ok || !response2.ok) {
+            alert('Erro ao consultar ação!')
+        }
+
+        if (profile?.exchange === undefined || result?.d === null) {
+            alert('Ação não encontrada!')
+            return
+        }
+        
+        const stock = {
+            bolsa: profile.exchange.split(' ')[0],
+            codigo: codigo.value,
+            empresa: profile?.name || '',
+            valor: result.c * 100,
+            variacao: result.d,
+            nAcoes: nAcoes.value
+        }
+
+        const card = document.getElementById(codigo.value)
+
+        if (card) {
+            updateCard(stock)
+        } else{
+            addCard(stock)
+        }
+    } catch(error){
+        alert('Erro ao consultar ação')
+        console.log('ERROR', error)
+    }
 
     event.target.reset()
     closeModal(null, 'addApiModal')
+}
+
+const cardEnter = (event) => {
+    const cardMenu = event.target.querySelector('.card-menu')
+    console.log(event.target)
+    cardMenu.style.display = 'flex'
+    console.log('cardEnter')
+}
+
+const cardLeave = (event) => {
+    const cardMenu = event.target.querySelector('.card-menu')
+    console.log(event.target)
+    cardMenu.style.display = 'none'
+    console.log('cardLeave')
+}
+
+const removeCard = (event) => {
+    event.target.closest('.card-ticker').remove()
+}
+
+const openEditModal = (event) => {
+    const card = event.target.closest('.card-ticker')
+
+    const inputBolsa = document.getElementById('e-bolsa')
+    inputBolsa.value = card.querySelector('header h2 span').innerText.replace(': ')
+
+    const inputCodigo = document.getElementById('e-codigo')
+    inputCodigo.value = card.querySelector('header h2').innerText.split(': ')[1]
+
+    const inputEmpresa = document.getElementById('e-empresa')
+    inputEmpresa.value = card.querySelector('header h1').innerText
+
+    const inputValor = document.getElementById('e-valor')
+    inputValor.value = card.querySelector('main p ').dataset.valor
+
+    const inputVariacao = document.getElementById('e-variacao')
+    inputVariacao.value = card.querySelector('main span:first-child').dataset.variacao
+
+    const inputAcoes = document.getElementById('e-nAcoes')
+    inputAcoes.value = card.querySelector('footer div p').innerText
+
+    openModal('editFormModal')
 }
